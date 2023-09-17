77bace017d67b994bfeeea1f3d2fce0665442654
Sebastian <hbhbnr@posteo.net>
2023-09-17 23:46:43 +0200

    Cache Fortran badge

--- cache_badges.sh
+++ cache_badges.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Python
 for version in 3.7 3.8 3.9 3.10 3.11; do
     curl --silent --show-error "https://img.shields.io/badge/Python-${version}-blue?logo=python&logoColor=white" > docs/badges/Python-${version}-blue-python-white.svg
+done
+
+# Fortran
+for version in 2018; do
+    curl --silent --show-error "https://img.shields.io/badge/Fortran-${version}-blue?logo=fortran&logoColor=white" > docs/badges/Fortran-${version}-blue-fortran-white.svg
 done
